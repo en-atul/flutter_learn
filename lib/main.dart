46b283476393029ab96c83bbd87af651e73f45ff
atul <atul@wellnite.com>
2021-12-27 00:14:18 +0530

    fetch movies from api

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,205 @@
+//https://api.nasa.gov/planetary/apod?api_key=WYADYvqRqLHWEEJbooogmsCg82AnA3UOEoqeXQ3K
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MoviesApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MoviesListing(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MoviesProvider {
+  static const String imagePathPrefix = 'https://image.tmdb.org/t/p/w500/';
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  //REPLACE: Replace YOUR_API_KEY with your API key
+  static const apiKey = "25c50fb149b3910e17ace24569beca98";
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  static Future<Map> getJson() async {
+    const apiEndPoint =
+        "http://api.themoviedb.org/3/discover/movie?api_key=$apiKey&sort_by=popularity.desc";
+    final apiResponse = await http.get(Uri.parse(apiEndPoint));
+    return json.decode(apiResponse.body);
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MoviesListing extends StatefulWidget {
+  const MoviesListing({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
+  @override
+  _MoviesListingState createState() => _MoviesListingState();
+}
+
+class _MoviesListingState extends State<MoviesListing> {
+  List<MovieModel> movies = [];
+
+  fetchMovies() async {
+    var data = await MoviesProvider.getJson();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      List<dynamic> results = data['results'];
+      for (var element in results) {
+        movies.add(MovieModel.fromJson(element));
+      }
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    //This method is called once when the widet starts first time
+    fetchMovies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    //Note- Call fetchMovies once from initState()
+    //fetchMovies();
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: ListView.builder(
+        // ignore: unnecessary_null_comparison
+        itemCount: movies == null ? 0 : movies.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+
+            //NEW CODE: New way to display title
+            //Title is being accessed as below rather `movies[index]["title"]`
+            child: MovieTile(movies, index),
+          );
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+//NEW CODE: MovieTile object to render visually appealing movie information
+class MovieTile extends StatelessWidget {
+  final List<MovieModel> movies;
+  // ignore: prefer_typing_uninitialized_variables
+  final index;
+
+  // ignore: use_key_in_widget_constructors
+  const MovieTile(this.movies, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          //Resizing image poster based on the screen size whenever image's path is not null.
+//Resizing image poster based on the screen size whenever the image's path is not null.
+          // ignore: unnecessary_null_comparison
+          movies[index].poster_path != null
+              ? Container(
+                  //Making image's width to half of the given screen size
+                  width: MediaQuery.of(context).size.width / 2,
+
+                  //Making image's height to one fourth of the given screen size
+                  height: MediaQuery.of(context).size.height / 4,
+
+                  //Making image box visually appealing by dropping shadow
+                  decoration: BoxDecoration(
+                    //Making image box slightly curved
+                    borderRadius: BorderRadius.circular(10.0),
+                    //Setting box's color to grey
+                    color: Colors.grey,
+
+                    //Decorating image
+                    image: DecorationImage(
+                        image: NetworkImage(MoviesProvider.imagePathPrefix +
+                            movies[index].poster_path),
+                        //Image getting all the available space
+                        fit: BoxFit.cover),
+
+                    //Dropping shadow
+                    // ignore: prefer_const_literals_to_create_immutables
+                    boxShadow: [
+                      const BoxShadow(
+                          //grey colored shadow
+                          color: Colors.grey,
+                          //Applying softening effect
+                          blurRadius: 3.0,
+                          //move 1.0 to right (horizontal), and 3.0 to down (vertical)
+                          offset: Offset(1.0, 3.0)),
+                    ],
+                  ),
+                )
+              : Container(), //Empty container when image is not available
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              movies[index].title,
+              style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black),
+            ),
+          ),
+          //Styling movie's description text
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              movies[index].overview,
+            ),
+          ),
+          Divider(color: Colors.grey.shade500),
+        ],
+      ),
+    );
+  }
+}
+//JSON response is converted into MovieModel object
+
+class MovieModel {
+  final int id;
+  final num popularity;
+  // ignore: non_constant_identifier_names
+  final int vote_count;
+  final bool video;
+  // ignore: non_constant_identifier_names
+  final String poster_path;
+  // ignore: non_constant_identifier_names
+  final String backdrop_path;
+  final bool adult;
+  // ignore: non_constant_identifier_names
+  final String original_language;
+  // ignore: non_constant_identifier_names
+  final String original_title;
+  // ignore: non_constant_identifier_names
+  final List<dynamic> genre_ids;
+  final String title;
+  // ignore: non_constant_identifier_names
+  final num vote_average;
+  final String overview;
+  // ignore: non_constant_identifier_names
+  final String release_date;
+
+  MovieModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        popularity = json['popularity'],
+        vote_count = json['vote_count'],
+        video = json['video'],
+        poster_path = json['poster_path'],
+        adult = json['adult'],
+        original_language = json['original_language'],
+        original_title = json['original_title'],
+        genre_ids = json['genre_ids'],
+        title = json['title'],
+        vote_average = json['vote_average'],
+        overview = json['overview'],
+        release_date = json['release_date'],
+        backdrop_path = json['backdrop_path'];
 }
